e70edf578790f398dbb688a7f183b3e60d1c8d5f
Justin Hager <coachjustinhager@gmail.com>
2020-11-25 13:23:53 -0800

    adds sort by app title test

--- test/app.test.js
+++ test/app.test.js
@@ -16,11 +16,46 @@ describe('App module', () => {
                 .expect(400)
         })
         // returns an error if sort does not have a value
-        // returns an error if genre is not a valid type
         // returns an error if genre does not have a value
-        // filters by genre correctly
-        // sorts by app correctly
-        // it sorts by rating correctly
+
+        it('returns an error if genre is not a valid type', () => {
+
+        })
+
+        it('filters by genre', () => {
+
+        })
+        it('sorts by app title', () => {
+            return supertest(app)
+                .get('/apps')
+                .query({ genres: 'Card' })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .then(res => {
+                    expect(res.body).to.be.an('array');
+                    let sorted = true;
+
+                    let i = 0;
+
+                    while (i < res.body.length -1 ){
+                        const itemAtI = res.body[i];
+                        const itemAtIPlus1 = res.body[i + 1];
+
+                        if (itemAtIPlus1.App <itemAtI){
+                            //items were not sorted correctly
+                            sorted = false;
+                            break; //exit the loop
+                        }
+                        i++;
+                    }
+                    expect(sorted).to.be.true;
+
+
+                })
+        })
+        it('it sorts by rating', () => {
+
+        })
     })
 
 })
